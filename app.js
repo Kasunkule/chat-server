82c4a931d3ba60793af7897540059daf2b4474f2
Kasun Rasanjana <141154279+Kasunkule@users.noreply.github.com>
2023-08-29 16:01:04 +0530

    created the server
    15th video is finished

--- app.js
+++ app.js
@@ -10,7 +10,28 @@ const mongosantize = require("express-mongo-sanitize");
 
 const bodyParser = require("body-parser");
 
+const xss = require("xss");
+
+const cors = requre("cors");
+
 const app = express();
+
+app.use("/tawl", limiter);
+
+app. use(express.urlencoded({
+    extended: true,
+}));
+
+app.use(mongosanitize());
+
+app.use(xss());
+
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "PATCH" , "POST", "DELETE", "PUT"],
+    credentials: true,
+}));
+
 
 app.une(express.json({limit: "10KB"}));
 app.use(bodyParser.urlencoded({extended: true}));
@@ -28,12 +49,5 @@ const limited = ratelimit(
         widowMs: 60 * 60 *1000,
         message:"Too many requests from this IP, Please try again in an hour",
 });
-
-app.use("/tawl", limiter);
-
-app. use(express.urlencoded({
-    extended: true,
-}))
-
 
 module.exports = app;
