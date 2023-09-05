54df4999feba1494ed8a5a27a324c4ca92083855
Kasun Rasanjana <141154279+Kasunkule@users.noreply.github.com>
2023-09-05 14:08:29 +0530

    16th video 20:00

--- server.js
+++ server.js
@@ -1,11 +1,40 @@
 const app = require("./app");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config({ path: "./config.env" });
+
+process.on("uncaughtException", (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
 
 const http = require("http");
 
 const server = http.createServer(app);
 
+const DB = process.env.DBURI.replace("<password>", process.env.DBPASSWORD);
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreatIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+}).then((con) => {
+    console.log("DB connection is successful");
+}).catch((err) => {
+    console.log(err);
+});
+
 const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
-    console.log ('App running on port ${port}');
+    console.log('App running on port ${port}');
+});
+
+process.on("inhandledRejection", (err) => {
+    console.log(err);
+    server.close(() => {
+        process.exxit(1);
+    });
 });
